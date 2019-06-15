8dd6ef91660d899febc3aabe7497ddbe9fb37a24
Fabian <fabian_57@yahoo.it>
2019-06-15 22:37:41 +0200

    Added port choise

--- ArduinoTester.py
+++ ArduinoTester.py
@@ -4,19 +4,35 @@ from ArduinoComunication import serial_ports
 
 def main():
     ports = serial_ports()
-    print('Available ports:' + str(ports))
     if ports == None or len(ports) == 0:
         print('Unable to find available ports')
         exit()
-    print('Choosing first port: ' + str(ports[0]))
-    connector = ArduinoConnector(ports[0])
+    port = choosePort(ports)
+    connector = ArduinoConnector(port)
+    print('Initializing serial port...')
     connector.initialize()
-
     print("Sending some bytes...")
     connector.write(b'abcdefg')
 
     print("All bytes sent")
     exit()
+
+
+def choosePort(ports):
+    exit = False
+    selected_index = 0
+    while not exit:
+        for i, p in enumerate(ports):
+            print('Port '+str(i)+": " + str(p))
+        number = input("Choose the port (type number): ")
+        try:
+            selected_index = int(number)
+            if 0 <= selected_index < len(ports):
+                exit = True
+        except ValueError:
+            pass
+
+    return ports[selected_index]
 
 
 if __name__ == "__main__":
