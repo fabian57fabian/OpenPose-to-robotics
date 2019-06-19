6a16f24a6fec3672bc76e268ee12ed150f2c86a7
pisalore <lorenzo.pisaneschi@gmail.com>
2019-06-19 12:46:32 +0200

    Updated files

--- IVA_project.py
+++ IVA_project.py
@@ -3,14 +3,10 @@ import sys
 from sys import *
 import os
 import time
-
 from steering import steering_angle
 
-
-#importo openpose, codice preso dal tutorial
 dir_path = os.path.dirname(os.path.realpath(__file__))
 try:
-    # Windows Import
     if platform == "win32":
         # Change these variables to point to the correct folder (Release/x64 etc.)
         sys.path.append(dir_path + '/../../python/openpose/Release');
@@ -26,24 +22,16 @@ except ImportError as e:
     print('Error: OpenPose library could not be found. Did you enable `BUILD_PYTHON` in CMake and have this Python script in the right folder?')
     raise e
 
-
-#settaggio parametri op
+#parameters op
 params = dict()
 params["model_folder"] = "../../../models/"
 params["number_people_max"] = 1
 params["body"] = 1
 params["alpha_pose"] = 1
 
-#salvo i risultati della posizione dello scheletro in formato json, abilitndo questa flag
-#params["write_json"] = "IVA_pose_estimation_results"
+# json saving results
+# params["write_json"] = "IVA_pose_estimation_results"
 
-#hand detection. NB: la frame rate si abbassa dimezzandosi
-#params["hand"] = True
-#params["hand_scale_number"] = 6
-#params["hand_scale_range"] = 0.4
-
-#stampa dei parametri
-#print(params)
 try:
 
     # Starting OpenPose
@@ -51,41 +39,43 @@ try:
     opWrapper.configure(params)
     opWrapper.start()
 
-    #alloco arrays dove memorizzo i keypoints che mi servono per i gesti, per cui ho previsto solo i polsi
+    # Wirsts keypoints arrays
     RightWirst_x = []
     RightWirst_y = []
     LeftWirst_x = []
     LeftWirst_y = []
 
-    #variabile di stato della macchina, può avere valore 0 (stop), 1 (backward), 2 (forward) (INT)
+    # Car state: 0 (stop), 1 (backward), 2 (forward) (INT)
     status = 0
-    #variabile che memorizza frame per frame l'angolo di sterzo (FLOAT)
+    # Steering Angle
     steeringAngle = 0.0
-    #velocità in funzione dell'altezza delle mani: ACC-> y di una delle due mani, SX-> altezza mano sx, DX-> altezza mano dx
-    speed = 0.0
+    # Speed
+    speed = 0
 
-    #avvio la webcam con opencv con il metodo VideoCapture. Il paametro 0 indica che verrà aperta la webcam installata
-    #nel laptop; con il flag WINDOWS_NORMAL posso fare resize 'dinamico' della finestra durante l'esecuzione del programma
+    # Start webcam with VideoCapture. 0 -> use default webcam
+    # WINDOWS_NORMAL dynamic window resize at running time
+    # resizeWindow output windows webcam dimension. RESOLUTION -> 4:3
     cv2.namedWindow('DETECTED KEYPOINTS', cv2.WINDOW_NORMAL)
-    #con resizeWindow indico la dimensione di partenza della finestra dell'output della webcam. RISOLUZIONE SCELTA-> 4:3
     cv2.resizeWindow('DETECTED KEYPOINTS', 1000, 750)
     stream = cv2.VideoCapture(0)
 
-    #Counter dei frame; mi serve per indicizzare i frame prodotti durante la computazione e calcolare frame rate
+    # Frame counter
     counter = 0
-    #Conteggio del tempo di esecuzione dello script
+    # Execution time
     start = time.time()
 
     while True:
-        # leggo i frame ottenuti dalla webcam e salvo frame per frame (frame.png viene processato con openpose)
+        # Frame reader. Each frame will be processed by OpenPose
         ret, img = stream.read()
-        #flip output dell'immagine catturata dalla webcam
+        # Output flip
         cv2.flip(img, 1, img)
-        #disegno linea al di sotto della quale la macchina è in stop, scrivo per interfaccia utente
+        # Stop Line
         cv2.line(img, (160, 380), (480, 380), (0, 0, 255), thickness=3)
         cv2.putText(img, 'STOP', (260,420), cv2.FONT_HERSHEY_TRIPLEX, 1.5, (0, 0, 255), thickness=2)
         cv2.putText(img, 'B', (70, 420), cv2.FONT_HERSHEY_TRIPLEX, 1.5, (0, 255, 0), thickness=2)
         cv2.putText(img, 'F', (550, 420), cv2.FONT_HERSHEY_TRIPLEX, 1.5, (255, 0, 0), thickness=2)
+        # Speedometer
+        cv2.putText(img, str(speed), (500, 50), cv2.FONT_HERSHEY_TRIPLEX, 1, (0, 0, 0), thickness=2)
 
         if(status == 0):
             cv2.putText(img, 'STOP MODE', (20, 30), cv2.FONT_HERSHEY_TRIPLEX, 0.7, (0, 0, 255), thickness=2)
@@ -94,24 +84,18 @@ try:
         if (status == 2):
             cv2.putText(img, 'FORWARD MODE', (20, 30), cv2.FONT_HERSHEY_TRIPLEX, 0.7, (255, 0, 0), thickness=2)
 
-        # disegno i rettangoli per Backward e Forward
+        # Backward/Forward zones
         cv2.rectangle(img, (0, 380), (160, 480), (0, 255, 0), thickness=2)
         cv2.rectangle(img, (480, 380), (640, 480), (255, 0, 0), thickness=2)
-        #cattura del frame i-esimo
-        cv2.imwrite("frame.png", img)
+        # Frame writer", img)
 
-        #processo i frame, utilizzando la classe datum, che serve per leggere e scrivere con gli oggetti openpose
-        #e openCV
+        # Frame processor by OpenPose (datum class)
         datum = op.Datum()
         imageToProcess = cv2.imread("frame.png")
         datum.cvInputData = imageToProcess
         opWrapper.emplaceAndPop([datum])
 
-        #print dei vari keypoints
-        #print("Body keypoints: \n" + str(datum.poseKeypoints))
-
-        #Controllo se trovo correttamente una persona con la webcam
-        #TODO: MIGLIORARE la verifica della presenza della persona cmagari con un ciclo while...
+        # Detecting people control
         if(str(datum.poseKeypoints) == str(2.0) or str(datum.poseKeypoints) == str(0.0)
                 or str(datum.poseKeypoints) == str(1e-45) ):
             print('NO DETECTED PEOPLE, YOU SHOULD GO IN FRONT OF YOUR WEBCAM')
@@ -119,7 +103,6 @@ try:
 
             RightWirst_x.append(0.0)
             RightWirst_y.append(0.0)
-
             LeftWirst_x.append(0.0)
             LeftWirst_y.append(0.0)
 
@@ -129,11 +112,11 @@ try:
             LeftWirst_x.append(datum.poseKeypoints[0][4][0])
             LeftWirst_y.append(datum.poseKeypoints[0][4][1])
 
-        #se angolo > 0, sterzo a sx, altrimenti se angolo < 0 sterzo a destra
+        # steeringAngle > 0 SX, steeringAngle < 0, DX
         steeringAngle = steering_angle(LeftWirst_x[counter], -LeftWirst_y[counter],
                                        RightWirst_x[counter], -RightWirst_y[counter])
 
-        #gestione senso di marcia e stop
+        # Direction and Stop
         if ((status == 0 and 380 < LeftWirst_y[counter] < 480 and 0 < LeftWirst_x[counter] < 160)
                 and RightWirst_y[counter] > 380):
             status = 1
@@ -153,46 +136,45 @@ try:
                     status = 0
                     print('------STOP------', status)
 
-        #gestione dei vari gesti: stop, accelerazione, svolta a dx e sx
-
-
+        # Gestures detection
         if(status != 0 and -10.0 < steeringAngle < 10.0
                 and RightWirst_y[counter] < 380.0 and LeftWirst_y[counter] < 380.0):
-            speed = 380 - max(RightWirst_y[counter], LeftWirst_y[counter])
+
+            speed = int(380 - max(RightWirst_y[counter], LeftWirst_y[counter]))
             print('ACCELLERATION. STATUS: ', status, '. SPEED:  ', speed)
         else:
             if(status != 0 and 10.0 < steeringAngle < 90.0
                     and RightWirst_y[counter] < 380.0 and LeftWirst_y[counter] < 380.0):
-                speed = 380 - max(RightWirst_y[counter], LeftWirst_y[counter])
-                print('TURN LEFT---. STATUS: ', status, '. SPEED:  ', speed, '. ANGLE: ', steeringAngle)
+                speed = int(380 - max(RightWirst_y[counter], LeftWirst_y[counter]))
+                print('TURN LEFT----. STATUS: ', status, '. SPEED:  ', speed, '. ANGLE: ', steeringAngle)
             else:
                 if( status != 0 and -90.0 < steeringAngle < -10.0
                         and RightWirst_y[counter] < 380.0 and LeftWirst_y[counter] < 380.0):
-                    speed = 380 - max(RightWirst_y[counter], LeftWirst_y[counter])
+                    speed = int(380 - max(RightWirst_y[counter], LeftWirst_y[counter]))
                     print('TURN RIGHT---. STATUS: ', status, '. SPEED:  ', speed, '. ANGLE: ', steeringAngle)
 
-        #mostro graficamente sull'output della webcam lo skeleton di openpose
+        # Output with OpenPose skeleton
         cv2.imshow('DETECTED KEYPOINTS', datum.cvOutputData)
         counter = counter + 1
 
-        # con il tasto q, Q == quit, interrompo il programma; NB: waitKey deve essere SEMPRE a 1
+        # q, Q == quit, STOP SCRIPT; NB: waitKey MUST BE 1
         key = cv2.waitKey(1)
         if key == ord('q') or key == ord('Q'):
             break
 
     end = time.time()
-    #rilascio le risorse di cv2
+    # Resources release
     stream.release()
     cv2.destroyAllWindows()
     total_time = end - start
 
-    #tempo e fps
+    # Time and fps
     print('-----------------------------------------------------------------------------------------------------------')
     print('Tempo totale di esecuzionde dello script : ', total_time)
     print('FPS: ', counter / total_time)
     print('-----------------------------------------------------------------------------------------------------------')
 
-#errore nell'allocazione di oggetto openpose
+# Openpose obj allocation error
 except Exception as e:
     # print(e)
     sys.exit(-1)
